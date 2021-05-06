28c42af2b6ecf7019aff7a741b09b5d69397f08f
felipebart10 <64552706+felipebart10@users.noreply.github.com>
2021-05-05 22:34:54 -0300

    (2021-05-05) Sudoku online solution

--- Top interview questions-easy/Array/Valid Soduku-WIP.py
+++ Top interview questions-easy/Array/Valid Soduku-WIP.py
@@ -1,10 +1,43 @@
-def isValidSudoku(matriz):
-    for linhas in matriz:
-        for elemento in linhas:
-            if linhas.count(elemento) > 1:
-                return False
-    
-    
-        
+def isValidSudoku(matrix):
+    linhaAtual = 0
+    colunaAtual = 0
+
+    while linhaAtual < 9:
+        while colunaAtual < 9:
+            i = 0
+            j = 0
+            temp = []
+            elementoTestado = matrix[linhaAtual][colunaAtual]
+            if elementoTestado != ".":
+                temp.append(elementoTestado)
+                while i < 9:
+                    while j < 9:
+                        if matrix[i][j] == elementoTestado:
+                            temp.append(elementoTestado)
+                        j += 1
+                        
+                        
+                
 
 
+                
+                
+
+    
+
+    
+
+
+sudoku = [
+    [".", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", "8", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+]
+
+print(isValidSudoku(sudoku))
